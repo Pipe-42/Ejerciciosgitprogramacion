033ce421e5365ccf20fa87a56e5b585707921c35
Pipe-42 <elFelipe227@gmail.com>
2025-08-28 13:02:03 -0500

    Cambios

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("comandos");
+        System.out.println("1.git status - ESTADO RAMA");
+        System.out.println("2.git add . - AGRGEGAR ARCHIVOS");
+        System.out.println("3.git commit -m - GUARDAR LOCAL");
+        System.out.println("4.git puhs origin - subir cambios");
+        System.out.println("5.git clone - clonar proyecto");
+        System.out.println("6.git branch - ver las ramas");
+        System.out.println("7.git fetch - ramas remotas");
+        System.out.println("8.git switch -c - crear y posicionarse sobre la rama");
+    }
+
+    }
